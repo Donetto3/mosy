0b99bc439e89839797e072ae7729fc2386f432c6
donatello3 <twister.molly@gmail.com>
2025-07-10 01:00:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
